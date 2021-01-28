ff52c9c1b844b3afccfbf2086b2aacf3d82ac691
cori <heycorihey@icloud.com>
2021-01-28 22:07:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,12 +1,12 @@
 # don't forget to add: require 'pry'
 require 'pry'
-def start_game("Rocky", "Bullwinkle")
-  "Hello #{player1} & #{playar2}"
+def start_game(player1, player2)
+  "Hello #{player1} & #{player2}"
   binding.pry
   puts 'hello'
 end
 
-def play_game("Jay-Z", "Beyonce")
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
   binding.pry
   puts 'hello'
