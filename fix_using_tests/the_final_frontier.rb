85f1d9f0dcd353137e6bd084716c8e8b95f49fcc
cori <heycorihey@icloud.com>
2021-01-28 22:14:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -11,6 +11,5 @@ end
 
 def engage
   date = generate_star_date
-  
-  binding.pry
+  puts state_log(date)
 end
